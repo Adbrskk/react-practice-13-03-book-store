861637058b1ec034d0c1f9ac9bc4faa50c04b26d
Adbrskk <aedobroskok@gmail.com>
2026-03-13 13:46:58 +0100

    add reducer and store for book catalog

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import store from "./redux/store";
 
 function App() {
-
-  console.log("Начальное состояние", store.getState());
+  console.log("Начало:", store.getState());
 
   store.dispatch({
     type: "BOOK_ADD",
     payload: {
       title: "Гарри Поттер",
       author: "Дж. Ролинг",
-      year: 1967
-    }
+      year: 1967,
+    },
   });
+
+  console.log("После добавления Гарри Поттера:", store.getState());
 
   store.dispatch({
     type: "BOOK_ADD",
     payload: {
       title: "1984",
       author: "Оруэлл",
-      year: 1949
-    }
+      year: 1949,
+    },
   });
+
+  console.log("После добавления 1984:", store.getState());
 
   const firstBookId = store.getState().books[0].id;
 
   store.dispatch({
     type: "BOOK_REMOVE",
-    payload: firstBookId
+    payload: firstBookId,
   });
+
+  console.log("После удаления первой книги:", store.getState());
 
   const secondBook = store.getState().books[0];
 
@@ -35,21 +40,23 @@ function App() {
     type: "BOOK_UPDATE_INFO",
     payload: {
       id: secondBook.id,
-      year: 1948
-    }
+      year: 1948,
+    },
   });
+
+  console.log("После обновления года:", store.getState());
 
   store.dispatch({
     type: "BOOK_TOGGLE_AVAILABILITY",
-    payload: secondBook.id
+    payload: secondBook.id,
   });
 
-  console.log("Финальное состояние", store.getState());
+  console.log("После переключения доступности:", store.getState());
 
   return (
     <div>
       <h1>Book Catalog</h1>
-      <p>Откройте консоль браузера</p>
+      <p>Открой консоль браузера</p>
     </div>
   );
 }
